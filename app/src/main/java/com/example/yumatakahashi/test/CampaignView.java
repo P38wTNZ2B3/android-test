101a5a1a29cea13ef6cfcd3ce85cac32c1f96d94
P38wTNZ2B3 <yuma1f@gmail.com>
2015-05-16 01:33:28 +0900

    refactoring list view page

--- app/src/main/java/com/example/yumatakahashi/test/CampaignView.java
+++ app/src/main/java/com/example/yumatakahashi/test/CampaignView.java
@@ -40,51 +40,6 @@ public class CampaignView extends Fragment implements AbsListView.OnScrollListen
         return campainview;
     }
 
-    private final static String[] s_tmp = {
-            //"https://test-yuma.s3.amazonaws.com/test/droid.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            //"https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            /*"https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",*/
-
-
-/*
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png",
-*/
-            "https://s3-ap-northeast-1.amazonaws.com/test-yuma/test/droid_blue.png"
-    };
-
     String[][] s = {
             {
                     "https://test-yuma.s3.amazonaws.com/test/droid.png",
@@ -153,51 +108,6 @@ public class CampaignView extends Fragment implements AbsListView.OnScrollListen
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        //String[] s = { "a", "b", "c", "d", "b", "c", "d", "b", "c", "d", "b",
-        //        "c", "d", "b", "c", "d", "b", "c", "d" };
-
-        //ArrayList<ModelClass> aList = new ArrayList<ModelClass>();
-/*
-        //int id = 0;
-        for (String s1 : s) {
-
-            aList.add(new ModelClass(s1));
-
-            //AsyncLoaderImageView imageView = (AsyncLoaderImageView)getActivity().findViewById(R.id.icon);
-            //imageView.setImageUrl(s1);
-
-            //getLoaderManager().initLoader(id, null, imageView);//ローダーを実行
-            //id++;
-
-        }
-*/
-
-/*        //RequestQueue queue = Volley.newRequestQueue(this);
-        RequestQueue mQueue = MyApplication.getInstance().getRequestQueue();
-
-        // Volley でリクエスト
-        String url = "http://www.google.com/uds/GnewsSearch?q=Obama&v=1.0";
-        mQueue.add(new JsonObjectRequest(Request.Method.GET, url, (String) null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-
-                        Log.d(TAG, "response : " + response.toString());
-                        Toast.makeText(getActivity(), response.toString(), Toast.LENGTH_LONG).show();
-                    }
-                }, null));
-        mQueue.start();
-  */
-        /*
-        ListView lv1 = (ListView) getActivity().findViewById(
-                R.id.campaignListView);
-        // lv1.setAdapter(new ArrayAdapter<String>(getActivity(),
-        // android.R.layout.simple_list_item_1,aList));
-        listViewPagerAdapter = new ListViewPagerAdapter(getActivity(), aList);
-        lv1.setAdapter(listViewPagerAdapter);
-        lv1.addFooterView(getFooter());
-        lv1.setOnScrollListener(this);
-*/
         ListView listView = getListView();
         listView.addFooterView(getFooter());
         listView.setAdapter(getAdapter());
@@ -222,11 +132,8 @@ public class CampaignView extends Fragment implements AbsListView.OnScrollListen
     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
         if (totalItemCount == firstVisibleItem + visibleItemCount) {
             additionalReading();
-            //readFlag = true;
         }
     }
-
-    //private boolean readFlag = false;
 
     private void additionalReading() {
         // 読み込み回数が最大値以上ならスキップ。フッタを消す
@@ -252,55 +159,10 @@ public class CampaignView extends Fragment implements AbsListView.OnScrollListen
 
             protected void onPostExecute(Void result) {
                 addListData();
-
                 mAdapter.notifyDataSetChanged();
                 //getListView().invalidateViews();
-                //invisibleFooter();
-
-                /*ListView list = getListView();
-                //int start = list.getFirstVisiblePosition();
-                //for (int i = start, j = list.getLastVisiblePosition(); i <= j; i++) {
-                for (int i = 6; i < 11; i++) {
-                    //if (target == list.getItemAtPosition(i)) {
-                    View view = list.getChildAt(i);
-                    list.getAdapter().getView(i, view, list);
-                    //break;
-                }*/
-
-                /*ListView list = getListView();
-                int start = list.getFirstVisiblePosition();
-                //for (int i = start, j = list.getLastVisiblePosition(); i <= j; i++) {
-                for (int i = start, j = start + 5; i < j; i++) {
-                    //if (target == list.getItemAtPosition(i)) {
-                        View view = list.getChildAt(i - start);
-                        list.getAdapter().getView(i, view, list);
-                        //break;
-                    //}
-                }*/
             };
         }.execute(Math.abs(new Random(System.currentTimeMillis()).nextLong() % 3000));
-        //}.execute(3000);
-        /*if (readFlag) {
-            return;
-        }
-        mTask = new AsyncTask<Long, Void, Void>() {
-            @Override
-            protected Void doInBackground(Long[] params) {
-                try {
-                    Log.d(TAG, "sleep..." + params[0]);
-                    Thread.sleep(params[0]);
-                } catch (Exception e) {
-
-                    e.printStackTrace();
-                }
-                return null;
-            };
-
-            protected void onPostExecute(Void result) {
-                addListData();
-                getListView().invalidateViews();
-            };
-        }.execute(Math.abs(new Random(System.currentTimeMillis()).nextLong() % 3000));*/
     }
 
     private void invisibleFooter() {
